4d2c7f25ccb45b5a4e1e4117e305a7bc167ab4b9
Gabriel Pineda <gabepineda6@gmail.com>
2020-11-14 18:04:20 -0800

    created a navbar module in my components file, a basic Bootstrap navbar after creating the react app

--- employee-directory/src/Components/Navbar.js
+++ employee-directory/src/Components/Navbar.js
@@ -1,1 +1,23 @@
 import React from 'react'
+
+function Nav() {
+    const styles ={
+        center : {
+            paddingLeft: 250
+        },
+        padding: {
+            paddingLeft : 330
+        }
+    }
+
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4" style={styles.center}>Employee Directory</h1>
+                <p className="lead"  style={styles.padding}>Search for any of your employees!</p>
+            </div>
+        </div>
+    );
+  }
+  
+  export default Nav;
